21cdad79b72aada94d7927d0280bbc3163ad3281
cmoyon <camille@taotesting.com>
2016-12-07 18:20:08 +0100

    REfacto rindex view

--- project/view/site/index.php
+++ project/view/site/index.php
@@ -1,6 +1,5 @@
 <body>
 	<?php
-    print_r($this->models);
-//	foreach($this->models['model']->getAttributes() as $id => $value)echo $id." => ".$value."<br>";
+	foreach($this->models['model']->getAttributes() as $id => $value)echo $id." => ".$value."<br>";
 	?>
 </body>
