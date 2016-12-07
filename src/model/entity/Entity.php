b49fb7bc5e984b526f195c3a739bfaa73854b1b1
julienBreem <julienbreem@gmail.com>
2016-12-07 18:04:55 +0100

    le fichier avait disparu

--- src/model/entity/Entity.php
+++ src/model/entity/Entity.php
@@ -3,35 +3,35 @@ namespace base\model\entity;
 
 class Entity
 {
-	protected $attributes;
-	protected $name;
-	protected $repositoryName = null;
-	
-	public function getRepositoryName()
-	{
-		return $this->repositoryName;
-	}
-	public function setRepositoryName($repositoryName)
-	{
-		$this->repositoryName = $repositoryName;
-	}
-	public function getName()
-	{
-		return $this->name;
-	}
-	public function setName($name)
-	{
-		$this->name = $name;
-	}
-	public function getAttributes()
-	{
-		return $this->attributes;
-	}
-	public function setAttributes($attributes)
-	{
-		$this->attributes = $attributes;
-	}
-	public function getAttribute($id)
+    protected $attributes;
+    protected $name;
+    protected $repositoryName = null;
+
+    public function getRepositoryName()
+    {
+        return $this->repositoryName;
+    }
+    public function setRepositoryName($repositoryName)
+    {
+        $this->repositoryName = $repositoryName;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+    public function setAttributes($attributes)
+    {
+        $this->attributes = $attributes;
+    }
+    public function getAttribute($id)
     {
         return $this->attributes[$id];
     }
